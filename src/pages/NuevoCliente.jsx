9143a2d94fa337553d0d0437dd8d57e1a125eff0
Daniel151505 <daniel150520011@gmail.com>
2023-04-02 19:19:42 -0500

    feat(NuevoCliente.jsx): Primeros pasos creando el formulario de clientes

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,5 +1,29 @@
+import { useNavigate } from "react-router-dom";
+
 function NuevoCliente() {
-  return <div>NuevoCliente</div>;
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Nuevo Cliente</h1>
+      <p className="mt-3">
+        Llena todos los campos para registrar un nuevo cliente
+      </p>
+
+      <div className="flex justify-end mt-3">
+        <button
+          className="bg-blue-800 text-white px-3 py-1 font-bold uppercasa"
+          onClick={() => navigate("/")}
+        >
+          Volver
+        </button>
+      </div>
+
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-5">
+        <p>Formulario aqui</p>
+      </div>
+    </>
+  );
 }
 
 export default NuevoCliente;
