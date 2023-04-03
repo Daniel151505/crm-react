a4a5d357ab2d67228d28174c5c4d4d0eb5092393
Daniel151505 <daniel150520011@gmail.com>
2023-04-02 19:26:50 -0500

    feat(Formulario.jsx, NuevoCliente.jsx): Agregando el Formulario

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import Formulario from "../components/Formulario";
 
 function NuevoCliente() {
   const navigate = useNavigate();
@@ -20,7 +21,15 @@ function NuevoCliente() {
       </div>
 
       <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-5">
-        <p>Formulario aqui</p>
+        <form>
+          <Formulario />
+
+          <input
+            type="submit"
+            className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
+            value="Registrar Cliente"
+          />
+        </form>
       </div>
     </>
   );
