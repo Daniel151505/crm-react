5f420de48b1c3a8aa2c06404dd564e18edf85ea7
Daniel151505 <daniel150520011@gmail.com>
2023-04-02 20:54:59 -0500

    feat(main.jsx, NuevoCliente.jsx): Creando un action para el formulario

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,5 +1,9 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Form } from "react-router-dom";
 import Formulario from "../components/Formulario";
+
+export function action() {
+  console.log("Submit al formulario..");
+}
 
 function NuevoCliente() {
   const navigate = useNavigate();
@@ -21,7 +25,7 @@ function NuevoCliente() {
       </div>
 
       <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-5">
-        <form>
+        <Form method="post">
           <Formulario />
 
           <input
@@ -29,7 +33,7 @@ function NuevoCliente() {
             className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
             value="Registrar Cliente"
           />
-        </form>
+        </Form>
       </div>
     </>
   );
