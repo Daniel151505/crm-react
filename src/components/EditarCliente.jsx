e55a313ccdeb40874299b1948b8207724365ff4c
Daniel151505 <daniel150520011@gmail.com>
2023-04-04 17:13:13 -0500

    feat(main.jsx, EditarCliente.jsx, Formulario.jsx): Añadiendo el formulario para editar y autocompletar

--- src/components/EditarCliente.jsx
+++ src/components/EditarCliente.jsx
@@ -1,4 +1,6 @@
+import { Form, useNavigate, useLoaderData } from "react-router-dom";
 import { obtenerCliente } from "../data/clientes";
+import Formulario from "./Formulario";
 
 export async function loader({ params }) {
   const cliente = await obtenerCliente(params.clienteId);
@@ -11,7 +13,37 @@ export async function loader({ params }) {
   return cliente;
 }
 function EditarCliente() {
-  return <div>EditarCliente</div>;
+  const navigate = useNavigate();
+  const cliente = useLoaderData();
+
+  return (
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Editar Cliente</h1>
+      <p className="mt-3">
+        A continuación podrás modificar los datos de un cliente
+      </p>
+
+      <div className="flex justify-end mt-3">
+        <button
+          className="bg-blue-800 text-white px-3 py-1 font-bold uppercasa"
+          onClick={() => navigate("/")}
+        >
+          Volver
+        </button>
+      </div>
+
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-5">
+        <Form method="post" noValidate>
+          <Formulario cliente={cliente} />
+          <input
+            type="submit"
+            className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
+            value="Registrar Cliente"
+          />
+        </Form>
+      </div>
+    </>
+  );
 }
 
 export default EditarCliente;
